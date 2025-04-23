efe55faa8544760914f5e10033aad9a15cc3747c
Ameer0006 <158128124+Ameer0006@users.noreply.github.com>
2025-04-23 09:50:28 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>KazBot</h1>
+      <p className="description">Твой помощник в изучении казахского языка 🇰🇿</p>
+
+      <div className="buttons">
+        <button>📚 Начать обучение</button>
+        <button>🧠 Повторение</button>
+        <button>🎮 Игровой режим</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <footer>@kzKazakhbot</footer>
+    </div>
   )
 }
 
